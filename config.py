413b2178702c5a29aaedee79a9b7ffc7ca52e8d3
FutureSoftYT <76397237+FutureSoftYT@users.noreply.github.com>
2022-09-28 20:26:48 +0500

    asda

--- config.py
+++ config.py
@@ -5,4 +5,3 @@ env.read_env()
 
 BOT_TOKEN = env.str("BOT_TOKEN")  # Забираем значение типа str
 YADISK_TOKEN = env.str("YADISK_TOKEN")  # Забираем значение типа str
-ADMINS = env.list("ADMINS")  # Тут у нас будет список из админов
